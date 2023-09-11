0444be9491663cd38cc05971620522088bdf9268
S H-SuHan <suhanur35-448@diu.edu.bd>
2023-09-12 00:36:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navber from './components/Navber/Navber';
+
+function App() {
+
+  return (
+    <>
+      <Navber></Navber>
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Navber/Navber.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Navber = () => {
+  return (
+    <>
+      <div className="navbar bg-cyan-950 text-white flex justify-between items-center lg:px-24 lg:py-3">
+        <div className="">
+          <a className="font-extrabold text-2xl">Knowledge Cafe</a>
+        </div>
+        <div className="">
+          <label tabindex="0" className="btn btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src="./profile.png" />
+            </div>
+          </label>
+          <hr className="" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navber;
